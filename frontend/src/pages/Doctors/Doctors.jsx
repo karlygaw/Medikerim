540260ac6643a7905c977292df39b80197841ba9
Gulnaz Omirzakova <gulnaz.omirzakova@narxoz.kz>
2023-11-15 16:59:20 +0600

    second

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,7 +1,18 @@
-import React from "react"; //6.9k (gzipped: 2.7k)
+
+
+import DoctorCard from './../../components/Doctors/DoctorCard';
+import { doctors } from './../../assets/data/doctors';
 
 const Doctors = () => {
-    return <div>Doctors</div>;
+    return <>
+    <section className='bg-[#fff9ea]'>
+        <div className="contaner text-center">
+            <h2 className='heading'>Därıgerdı tabu</h2>
+            <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between"></div>
+        </div>
+    </section>
+    
+    </>;
 };
 
 export default Doctors;
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,7 +1,15 @@
-import React from "react"; //6.9k (gzipped: 2.7k)
+import { services } from '../assets/data/services';
+import ServiceCard from "../components/Services/ServiceCard";
 
 const Services = () => {
-    return <div>Services</div>;
+    return <section>
+        <div className="container">
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] '>
+        {services.map((item, index) => 
+        <ServiceCard item={item} index={index} key={index} />)}
+    </div>
+    </div>
+    </section>;
 };
 
 export default Services;
